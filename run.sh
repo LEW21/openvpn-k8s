4239b2095e30c67bc3664cba7546e868387b8c7f
Linus Lewandowski <linus@lew21.net>
2016-12-06 17:54:22 +0100

    Push DOMAIN too.

--- run.sh
+++ run.sh
@@ -59,7 +59,11 @@ EOF
 [ -n "$K8S_PODS" ] && echo "push \"route $(getroute $K8S_PODS)\"" >> /run/openvpn-server.conf
 [ -n "$K8S_SERVICES" ] && echo "push \"route $(getroute $K8S_SERVICES)\"" >> /run/openvpn-server.conf
 
-[ -n "$K8S_DNS" ] && echo "push \"dhcp-option DNS $K8S_DNS\"" >> /run/openvpn-server.conf
+if [ -n "$K8S_DNS" ]
+then
+    echo "push \"dhcp-option DNS $K8S_DNS\"" >> /run/openvpn-server.conf
+    echo "push \"dhcp-option DOMAIN cluster.local\"" >> /run/openvpn-server.conf
+fi
 
 iptables -t nat -A POSTROUTING -s $OVPN_CLIENTS -o eth0 -j MASQUERADE
 
